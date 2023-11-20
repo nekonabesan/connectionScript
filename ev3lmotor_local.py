2be8018bdd93237f147ad363c9cb35edc4adc56e
nekonabesan <sheepdogs_2@hotmail.com>
2023-11-20 23:49:57 +0900

    localスクリプト修正

--- ev3lmotor_local.py
+++ ev3lmotor_local.py
@@ -5,9 +5,10 @@ import numpy as np
 from decimal import *
 from threading import Event
 from sense_hat import SenseHat
-#from MotorDriver import *
+from MotorDriver import *
 from ConnectionSenseHat import *
 from modules.PCA9685 import PCA9685
+from multiprocessing import Value, Array, Process
 from gpiozero import RotaryEncoder, Button
 from gpiozero.pins.pigpio import PiGPIOFactory
 #from WitMotionSensorConnection import JY901S
@@ -24,55 +25,6 @@ PIN_ROTAR_A1 = 17
 PIN_ROTAR_A2 = 27
 PIN_ROTAR_D1 = 22
 PIN_ROTAR_D2 = 23
-
-direction = [
-    'forward',
-    'backward',
-]
-
-# PWM制御
-pwm = PCA9685(0x40, debug=False)
-pwm.setPWMFreq(50)
-
-class MotorDriver():
-    def __init__(self):
-        self.PWMA = 0
-        self.AIN1 = 1
-        self.AIN2 = 2
-        self.PWMB = 5
-        self.BIN1 = 3
-        self.BIN2 = 4
-
-    def MotorRun(self, motor, index, speed):
-        if speed > 100:
-            return
-        if(motor == 0):
-            pwm.setDutycycle(self.PWMA, speed)
-            if(index == direction[0]):
-                pwm.setLevel(self.AIN1, 0)
-                pwm.setLevel(self.AIN2, 1)
-            else:
-                pwm.setLevel(self.AIN1, 1)
-                pwm.setLevel(self.AIN2, 0)
-        else:
-            pwm.setDutycycle(self.PWMB, speed)
-            if(index == direction[0]):
-                pwm.setLevel(self.BIN1, 0)
-                pwm.setLevel(self.BIN2, 1)
-            else:
-                pwm.setLevel(self.BIN1, 1)
-                pwm.setLevel(self.BIN2, 0)
-
-    def MotorStop(self, motor):
-        if (motor == 0):
-            pwm.setDutycycle(self.PWMA, 0)
-        else:
-            pwm.setDutycycle(self.PWMB, 0)
-
-
-
-motor_driver = MotorDriver()
-pwm.setPWMFreq(50000000.0)
 
 # ロータリーエンコーダ初期化
 rotation_a = FWD
@@ -108,11 +60,6 @@ rotary_encoder_d.steps = 0
 rotary_encoder_d.when_rotated_clockwise = rotated_clockwise_d
 rotary_encoder_d.when_rotated_counter_clockwise = rotated_counter_clockwise_d
 
-before_count_a = 0
-before_count_d = 0
-rt_a_counter = 0
-rt_d_counter = 0
-
 '''
 obj = None
 device = None
@@ -130,7 +77,6 @@ obj.readConfig(device)
 # Data update event
 device.dataProcessor.onVarChanged.append(obj.onUpdate)
 '''
-prev_angle_y = 0
 
 # sense hatを初期化
 sense = SenseHat()
@@ -143,110 +89,133 @@ for i in range(360):
         angle.append(i - 360)
 
 # sense hatの指示値読み取りをスレッド化
-th_angle_y = 0
-def read_sense_hat_angle():
-    global th_angle_y
+def read_sense_hat_angle(th_angle_y):
+    global angle
     while True:
         orientation = sense.get_orientation_degrees()
-        th_angle_y = angle[round(orientation['pitch']) - OFFSET]
-th = threading.Thread(target=read_sense_hat_angle, args=(), name='th')
-th.start()
-
-# 積算開始
-start = time.time()
-
-# 制御パラメータ設定
-k1 = Decimal(str(338.05839545))
-k2 = Decimal(str(46.78625234))
-k3 = Decimal(str(10.95445115))
-k4 = Decimal(str(7.6766376))
-
-# モータ初期化
-direction_a = 0
-direction_d = 0
-motor_driver.MotorRun(0, direction[direction_a], SPEED)
-motor_driver.MotorRun(1, direction[direction_d], SPEED)
+        th_angle_y.value = angle[round(orientation['pitch']) - OFFSET]
 
 getcontext().prec = 28
-while True:
-    # 振子の回転角度を取得
-    angle_y = Decimal(str(th_angle_y))
-    delta_angle_y = Decimal(angle_y - prev_angle_y)
-    rt_a_counter = rotary_encoder_a.steps
-    rt_d_counter = rotary_encoder_d.steps
-    # １ステップ前からのモータ回転角度
-    delta_theta_a = Decimal(abs(abs(rt_a_counter) - abs(before_count_a)))
-    delta_theta_d = Decimal(abs(abs(rt_d_counter) - abs(before_count_d)))
-    if rotation_a == BCK:
-        delta_theta_a = -delta_theta_a
-    if rotation_d == BCK:
-        delta_theta_d = -delta_theta_d
-    # motorの振子に対する角度
-    if np.sign(delta_theta_a) == np.sign(delta_angle_y):
-        motor_a_angle = delta_theta_a - delta_angle_y
-    else:
-        motor_a_angle = delta_angle_y - delta_theta_a
-    if np.sign(delta_theta_d) == np.sign(delta_angle_y):
-        motor_d_angle = delta_theta_d - delta_angle_y
-    else:
-        motor_d_angle = delta_angle_y - delta_theta_d
-    delta_time = Decimal(time.time() - start)
+def request_controler(th_angle_y):
+    global rotation_a,rotation_d,rotary_encoder_a,rotary_encoder_d
+    # 積算開始
     start = time.time()
-    # 振子の角速度
-    if delta_angle_y == 0:
-        angular_velocity_y = Decimal(0)
-    else:
-        angular_velocity_y = Decimal(delta_angle_y/delta_time)
-    # モータの角速度
-    velocity_a = Decimal(delta_theta_a/delta_time)
-    velocity_d = Decimal(delta_theta_d/delta_time)
 
-    # U(t)を導出
-    ua = ((angle_y * k1) + (angular_velocity_y * k2) + (motor_a_angle * k3) + (velocity_a * k4))
-    ud = ((angle_y * k1) + (angular_velocity_y * k2) + (motor_d_angle * k3) + (velocity_d * k4))
+    before_count_a = 0
+    before_count_d = 0
+    rt_a_counter = 0
+    rt_d_counter = 0
+    prev_angle_y = 0
 
-    # 回転方向を設定
-    if ua < 0:
-        direction_a = BCK
-    else:
-        direction_a = FWD
+    direction = [
+        'forward',
+        'backward',
+    ]
 
-    if ud < 0:
-        direction_d = BCK
-    else:
-        direction_d = FWD
-    ua = abs(ua)
-    ud = abs(ud)
+    # PWM制御
+    pwm = PCA9685(0x40, debug=False)
+    pwm.setPWMFreq(50)
+    motor_driver = MotorDriver()
+    pwm.setPWMFreq(50000000.0)
 
-    # 系に対してPWMデューティ比として与える制御入力をスケーリング
-    if ua != U_MIN:
-        ua = round(Decimal(0.02) * ua)
-    if ud != U_MIN:
-        ud = round(Decimal(0.02) * ud)
+    # 制御パラメータ設定
+    k1 = Decimal(str(178.35900414))
+    k2 = Decimal(str(24.6536052))
+    k3 = Decimal(str(3.7106889))
+    k4 = Decimal(str(4.02940645))
 
-    # デューティ比の最大値を超過する場合は足切り
-    if ua > U_MAX:
-        ua = U_MAX
-    if ud > U_MAX:
-        ud = U_MAX
-    
-    print("angle_y  : " + str(angle_y)
-                + "\tangular_velocity_y  : " + str(angular_velocity_y) 
-                + "\t motor angle : " + str(motor_a_angle)
-                + "\t motor velocity : " + str(velocity_a)
-                + "\t motor delta_theta : " + str(delta_theta_a)
-                + "\t direction : " + direction[direction_a]
-                + "\t u(t)  : " + str(ua)
-                + "\t delta : " + str(delta_time))
+    # モータ初期化
+    direction_a = 0
+    direction_d = 0
+    motor_driver.MotorRun(pwm, 0, direction[direction_a], SPEED)
+    motor_driver.MotorRun(pwm, 1, direction[direction_d], SPEED)
 
-    # deleta theata計測用
-    before_count_a = rt_a_counter
-    before_count_d = rt_d_counter
-    prev_angle_y = angle_y
+    getcontext().prec = 28
+    while True:
+        # 振子の回転角度を取得
+        angle_y = Decimal(str(th_angle_y.value))
+        delta_angle_y = Decimal(angle_y - prev_angle_y)
+        rt_a_counter = rotary_encoder_a.steps
+        rt_d_counter = rotary_encoder_d.steps
+        # １ステップ前からのモータ回転角度
+        delta_theta_a = Decimal(abs(abs(rt_a_counter) - abs(before_count_a)))
+        delta_theta_d = Decimal(abs(abs(rt_d_counter) - abs(before_count_d)))
+        if rotation_a == BCK:
+            delta_theta_a = -delta_theta_a
+        if rotation_d == BCK:
+            delta_theta_d = -delta_theta_d
+        # motorの振子に対する角度
+        if np.sign(delta_theta_a) == np.sign(delta_angle_y):
+            motor_a_angle = delta_theta_a - delta_angle_y
+        else:
+            motor_a_angle = delta_angle_y - delta_theta_a
+        if np.sign(delta_theta_d) == np.sign(delta_angle_y):
+            motor_d_angle = delta_theta_d - delta_angle_y
+        else:
+            motor_d_angle = delta_angle_y - delta_theta_d
+        delta_time = Decimal(time.time() - start)
+        start = time.time()
+        # 振子の角速度
+        if delta_angle_y == 0:
+            angular_velocity_y = Decimal(0)
+        else:
+            angular_velocity_y = Decimal(delta_angle_y/delta_time)
+        # モータの角速度
+        velocity_a = Decimal(delta_theta_a/delta_time)
+        velocity_d = Decimal(delta_theta_d/delta_time)
 
-    # モータへデューティ比を印加
-    motor_driver.MotorRun(0, direction[direction_a], ua)
-    motor_driver.MotorRun(1, direction[direction_d], ua)
-    # ハードウェア側の応答を見て待ち時間を設定
-    #time.sleep(0.002)
-    
+        # U(t)を導出
+        ua = ((angle_y * k1) + (angular_velocity_y * k2) + (motor_a_angle * k3) + (velocity_a * k4))
+        ud = ((angle_y * k1) + (angular_velocity_y * k2) + (motor_d_angle * k3) + (velocity_d * k4))
+
+        # 回転方向を設定
+        if ua < 0:
+            direction_a = BCK
+        else:
+            direction_a = FWD
+
+        if ud < 0:
+            direction_d = BCK
+        else:
+            direction_d = FWD
+        ua = abs(ua)
+        ud = abs(ud)
+
+        # 系に対してPWMデューティ比として与える制御入力をスケーリング
+        if ua != U_MIN:
+            ua = round(Decimal(0.02) * ua)
+        if ud != U_MIN:
+            ud = round(Decimal(0.02) * ud)
+
+        # デューティ比の最大値を超過する場合は足切り
+        if ua > U_MAX:
+            ua = U_MAX
+        if ud > U_MAX:
+            ud = U_MAX
+        
+        print("angle_y  : " + str(angle_y)
+                    + "\tangular_velocity_y  : " + str(angular_velocity_y) 
+                    + "\t motor angle : " + str(motor_a_angle)
+                    + "\t motor velocity : " + str(velocity_a)
+                    + "\t motor delta_theta : " + str(delta_theta_a)
+                    + "\t direction : " + direction[direction_a]
+                    + "\t u(t)  : " + str(ua)
+                    + "\t delta : " + str(delta_time))
+
+        # deleta theata計測用
+        before_count_a = rt_a_counter
+        before_count_d = rt_d_counter
+        prev_angle_y = angle_y
+
+        # モータへデューティ比を印加
+        motor_driver.MotorRun(pwm, 0, direction[direction_a], ua)
+        motor_driver.MotorRun(pwm, 1, direction[direction_d], ua)
+        # ハードウェア側の応答を見て待ち時間を設定
+        #time.sleep(0.002)
+
+if __name__ == "__main__":
+    th_angle_y = Value('i', 0)
+    p1 = Process(target=read_sense_hat_angle, args=[th_angle_y])
+    p2 = Process(target=request_controler, args=[th_angle_y])
+    p1.start()
+    p2.start()
